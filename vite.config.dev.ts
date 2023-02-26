9e1b209b15445612460a39cad609fc608bfc8694
xue mufan <xuemf@ffcs.cn>
2023-02-26 09:41:02 +0800

    conf: 开放启动端口配置

--- vite.config.dev.ts
+++ vite.config.dev.ts
@@ -5,20 +5,20 @@ export default mergeConfig(
   BasicConfig,
   defineConfig({
     mode: 'development',
-    // server: {
-    //   port: 3000, // 开发时占用3000端口
-    //   strictPort: true, // 端口被占用的时候直接退出，不移到下一个端口
-    //   open: true, // 启动时自动打开浏览器
-    //   proxy: {
-    //     '/api': {
-    //       target: '',
-    //       changeOrigin: true,
-    //       rewrite: (path) => path.replace(/^\/api/, '')
-    //     }
-    //   },
-    //   cors: true, // 开发时允许任何源
-    //   force: false // 为true的时候强制每次都进行依赖预构建
-    // },
+    server: {
+      port: 8080, // 开发时占用8080端口
+      strictPort: true, // 端口被占用的时候直接退出，不移到下一个端口
+      open: true, // 启动时自动打开浏览器
+      proxy: {
+        '/api': {
+          target: '',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      },
+      cors: true, // 开发时允许任何源
+      force: false // 为true的时候强制每次都进行依赖预构建
+    },
     build: {
       sourcemap: 'inline', // 指定生成的sourcemap的格式
       terserOptions: {
